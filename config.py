bd3fc0b1289033aae803a5454e9481a05fe7fc49
bov2 <slrklkw@catway.org>
2024-07-28 21:55:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botnwaf/music",
+    "https://github.com/bov2/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
